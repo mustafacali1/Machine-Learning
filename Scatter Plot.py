7e892b98484582b4202666adf6574aed80111d88
Mustafa Cali <85156046+mustafacali1@users.noreply.github.com>
2023-08-22 23:29:33 +0300

    Create Scatter Plot.py

---
+++ Scatter Plot.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+# Scatter Plot is another diagram where every value shown as a dot.
+
+# for drawing a scatter plot we need 2 arrays. first array will be x-asis, second array will be y-axis
+
+x = [5,7,8,7,2,17,2,9,4,11,12,9,6]
+
+y = [99,86,87,88,111,86,103,87,94,78,77,85,86]
+
+plt.scatter(x, y)
+plt.show()
