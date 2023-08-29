a36b0ca64096cc831e1b7fde335d3e6134ecd2a9
Mustafa Cali <mustafacali98@gmail.com>
2023-08-29 23:20:17 +0300

    Update 07 - Scale.py

--- 07 - Scale.py
+++ 07 - Scale.py
@@ -1,3 +1,5 @@
 import pandas
 from sklearn import linear_model
 from sklearn.preprocessing import StandardScaler
+
+# scale is a method that we can use to compare different values like meters and kilograms etc.
